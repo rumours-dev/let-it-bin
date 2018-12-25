d58c5b8d84b6507274b0a35dcd2bcc4e3d25f60b
KaaJaryi <5781292+KaaJaryi@users.noreply.github.com>
2018-12-25 13:16:18 +0100

    :sparkles: can get, update and create gists on github and friendpaste
    some cors problems persist for writeas (even for get) and glotio

--- apiBase.js
+++ apiBase.js
@@ -1,28 +1,64 @@
+const responseType = {
+    url: '',
+    id: '',
+    content: '',
+}
+
 const apiBase = {
     github: {
-        url: 'https://api.github.com/gists/',
+        url: 'https://api.github.com/gists',
+        authorizationNeeded: true,
         updateMethod: 'PATCH',
+        createMethod: 'POST',
+        deleteMethod: 'DELETE',
         headers: {
             GET: {
                 'Accept': 'application/vnd.github.v3+json',
                 'Accept-Charset' : 'utf-8',
-            }
+                'User-Agent': 'let-it-bin',
+            },
+            PATCH: {
+                'Content-Type': 'application/vnd.github.v3+json',
+                'User-Agent': 'let-it-bin',
+            },
+            POST: {
+                'Content-Type': 'application/vnd.github.v3+json',
+                'User-Agent': 'let-it-bin',
+            },
         },
         formatData: text => {
-            const gitfile = 'gistfile1.txt'
+            const gitsfile = 'gistfile1.txt'
 
             const json = Object.assign({}, {
-                'gistfile1.txt': {
-                    content: '',
+                files: {
+                    [gitsfile]: {
+                        content: '',
+                    }
                 }
             })
 
-            json[gitfile].content = text
+            json.files[gitsfile].content = text
             return json
         },
+        handleResponse: res => {
+            try {
+                const gitsfile = 'gistfile1.txt'
+                const response = Object.assign({}, responseType )
+
+                response.url = res.url
+                response.id = res.id
+                response.content = res.files[gitsfile].content
+
+                return response
+            } catch ( error ){
+                throw new Error( error )
+            }
+        }
+
     },
     friendpaste: {
-        url: 'https://friendpaste.com/',
+        url: 'https://friendpaste.com',
+        authorizationNeeded: false,
         updateMethod: 'PUT',
         createMethod: 'POST',
         headers: {
@@ -50,15 +86,46 @@ const apiBase = {
 
             return json
         },
+        handleResponse: ( res, url ) => {
+            try {
+                if( !res.snippet && !res.ok ) throw new Error()
+
+                const response = Object.assign({}, responseType )
+                response.url = res.url || url
+                response.id = res.id
+                if( res.snippet ) response.content = res.snippet
+
+                return response
+            } catch ( error ){
+                error.message = res.reason
+                throw error
+            }
+        }
     },
     glotio: {
-        url: 'https://snippets.glot.io/snippets/',
-        updateMethod: 'PUT',
+        url: 'https://snippets.glot.io/snippets',
+        updateMethod: 'PUT', //requires token
+        createMethod: 'POST', //requires token, except anonymous snippet
+        deleteMethod: 'DELETE', //retquire token
+        headers: {
+            GET: {
+                'Accept': 'application/json',
+                'Accept-Charset' : 'utf-8',
+            },
+            PUT: {
+                'Content-Type': 'application/json',
+                'Content-Length': '245'
+            },
+            POST: {
+                'Content-Type': 'application/json',
+                'Content-Length': '245'
+            },
+        },
         formatData: text => {
             const json = Object.assign({}, {
-                files: {
+                files: [{
                     content: ''
-                }
+                }]
             })
 
             json.files.content = text
@@ -67,8 +134,24 @@ const apiBase = {
         },
     },
     writeas: {
-        url: 'https://write.as/api/posts/',
-        updateMethod: 'POST',
+        url: 'https://write.as/api/posts',
+        updateMethod: 'POST', //requires token
+        createMethod: 'POST',
+        deleteMethod: 'DELETE', //requires token
+        headers: {
+            GET: {
+                'Accept': 'application/json',
+                'Accept-Charset' : 'utf-8',
+            },
+            PUT: {
+                'Content-Type': 'application/json',
+                'Content-Length': '245'
+            },
+            POST: {
+                'Content-Type': 'application/json',
+                'Content-Length': '245'
+            },
+        },
         formatData: text => {
             const json = Object.assign({}, {
                 body: ''
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fetch = require( 'node-fetch' )
 
 export default class LetItBin {
     constructor( service, auth = {}) {
+        this.__apibase = apiBase[service]
         this.__url = apiBase[service].url
 
         this.__auth = {
@@ -13,69 +14,98 @@ export default class LetItBin {
         }
 
         this.__service = service
-
-        if( auth.token )
-            this.__authorizationHeader = 'token ' + auth.token
-        else if( auth.username && auth.password )
-            this.__authorizationHeader = 'Basic ' + Base64.encode(
-                auth.username + ':' + auth.password
-            )
     }
 
     get( id ){
         const method = 'GET'
 
-        const url = this.getUrl( id )
+        const url = this.__url + '/' + id
 
         return this.request({ method, url })
     }
 
     update( id, newText ){
-        const data = apiBase[this.__service].formatData( newText )
+        const data = this.__apibase.formatData( newText )
 
-        const method = apiBase[this.__service].updateMethod
+        const method = this.__apibase.updateMethod
 
-        const url = this.getUrl( id )
+        const url = this.__url + '/' + id
 
         return this.request({ method, url, data })
     }
 
     create( text ) {
-        const data = apiBase[this.__service].formatData( text )
+        const data = this.__apibase.formatData( text )
 
-        const method = apiBase[this.__service].createMethod
+        const method = this.__apibase.createMethod
+
+        if( this.__service === 'github' ){
+            const url = this.__url
+            return this.request({ method, data, url })
+        }
 
         return this.request({ method, data })
-
     }
 
-    getUrl( id ) {
-        let url = this.__url
-        if( this.__service === 'github' )
-            url += this.__auth.username + '/'
-        url += id
+    async request({ method, url = this.__url, data, auth = true }){
+        try {
+            const useragent = 'user-agent'
+            let headers = Object.assign( this.__apibase.headers[method] )
+            headers[useragent] = 'https://github.com/KaaJaryi/let-it-bin'
+            if( this.__apibase.authorizationNeeded && auth )
+                headers.Authorization = this.getAuthorizationHeaders()
 
-        return url
-    }
+            const config = {
+                method,
+                headers,
+            }
+            if( data )
+                config.body = JSON.stringify( data )
 
-    async request({ method, url = this.__url, data }){
-        let headers = Object.assign( apiBase[this.__service].headers[method] )
-        if( this.__auth.token )
-            headers.Authorization = 'Token ' + this.__auth.token
-        if( this.__authorizationHeader )
-            headers.Authorization = this.__authorizationHeader
+            const response = await fetch( url, config )
+                .then( res => {
+                    if( res.status >= 200 && res.status < 300 )
+                        return res.json()
 
-        const config = {
-            method,
-            headers,
+                    const error = new Error( res.statusText || res.status )
+                    error.response = res
+                    throw error
+                })
+                .then( json => {
+                    return this.__apibase.handleResponse( json, url )
+                })
+
+            return response
+        } catch ( error ) {
+            throw new Error( error )
         }
-        if( data )
-            config.body = JSON.stringify( data )
-
-        const response = await fetch( url, config )
-            .then( res => res.json() )
-            .then( res => res )
-
-        return response
     }
+
+    getAuthorizationHeaders(){
+        const token = 'access_token'
+
+        if( this.__auth.token )
+            return 'Token ' + this.__auth.token
+
+        if( this.__service === 'github' ){
+            return 'Basic ' + Base64.encode(
+                this.__auth.username + ':' + this.__auth.password
+            )
+        }
+
+        if( this.__service === 'writeas' ){
+            const authResponse = this.request({
+                url: 'https://write.as/api/auth/login',
+                method: 'POST',
+                data: {
+                    alias:this.__auth.username,
+                    pass:this.__auth.password
+                },
+                auth: false
+            })
+            this.__auth.token = authResponse.data[token]
+            return 'Token' + this.__auth.token
+        }
+    }
+
 }
